8174cb8570cd43ed2b89134f42991c9099fd4705
Burak Aksar <baksar@bu.edu>
2018-11-17 22:18:56 -0500

    Dataset problem is solved and script is working

--- Matlab_Files/dataset_ordering.m
+++ Matlab_Files/dataset_ordering.m
@@ -62,11 +62,11 @@ shot_attempted_flag_unique=unique(T.(40),'stable');
 shot_made_flag_unique=unique(T.(41),'stable');
 
 
-num_ordered_features=zeros(length(T.(1)),36);
+num_ordered_features=zeros(length(T.(1)),32);
 
 a=1;
-for i=[1 3 4 5 6 7 8 9 11 12 13 15 16 17 18 19 20 21 22 23 24 26 ...
-        28 29 30 31 32 33 34 35 36 37 38 39 40 41]
+for i=[1 3 4 5 7 8 9 11 12 13 16 17 18 19 21 22 23 24 26 ...
+        28 29 30 32 33 34 35 36 37 38 39 40 41]
 
     if iscell(T.(i))
     s=string(T.(i));
